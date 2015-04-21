2a838f0e9e722eeebea3328fcd49d58b60264265
Douglas Costa <dodop3000@gmail.com>
2015-04-21 10:32:15 -0300

    functions to scape html

---
+++ public/javascripts/chat_ui.js
@@ -0,0 +1,7 @@
+function divEscpacedEle(message){
+  return $('<div></div>').text(message);
+}
+
+function divSystemContentEle(message){
+  return $('<div></div>').html('<i>' + message + '</i>');
+}
