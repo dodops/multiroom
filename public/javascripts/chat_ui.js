b9d2f58f94e98a57430c0614ac50a37ff27e7acb
Douglas Costa <dodop3000@gmail.com>
2015-04-26 18:51:00 -0300

    client logic

--- public/javascripts/chat_ui.js
+++ public/javascripts/chat_ui.js
@@ -5,3 +5,20 @@ function divEscpacedEle(message){
 function divSystemContentEle(message){
   return $('<div></div>').html('<i>' + message + '</i>');
 }
+
+function processUserInput(chatApp, socket){
+  var message = $('#send-message').val();
+  var systemMessage;
+
+  if(message.charAt(0) == '/'){
+    systemMessage = chatApp.processCommand(message);
+    if(systemMessage){
+      $('#messages').append(divSystemContentEle(systemMessage));
+    }
+  } else {
+    chatApp.sendMessage($('#room').text(), message);
+    $('#messages').append(divEscpacedEle(message));
+    $('#messages').scrollTop($('#messages').prop('scrollHeight'));
+  }
+  $('#send-message').val('');
+}
